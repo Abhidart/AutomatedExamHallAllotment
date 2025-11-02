7402d994484761f58f64defbeebe2caf1e674af7
nandhukpvr <nandhukrishnaperavoor@gmail.com>
2025-11-03 00:19:45 +0530

    added list of alloted students hall wise

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+import mysql.connector
 from flask import Flask, render_template,request,redirect,url_for
+
+
+
+    
 app= Flask(__name__)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",           # replace with your MySQL username
+    password="12345",  # replace with your MySQL password
+    database="exam_allotment")
+cursor = db.cursor()
 
 @app.route('/')
 def home():
@@ -27,6 +39,29 @@ def submit():
             'reg_num': reg_nums[i]
         })
     return render_template('results.html', students=students)
+
+
+@app.route('/rooms')
+def rooms():
+    cursor.execute("SELECT DISTINCT room_no FROM exam_halls")
+    all_rooms = cursor.fetchall()
+    return render_template('rooms.html', rooms=[room[0] for room in all_rooms])
+
+@app.route('/room/<room_no>')
+def room_students(room_no):
+    query = """
+        SELECT eh.id, eh.seat_no, s.id, s.name, s.branch, s.register_no
+        FROM exam_halls eh
+        JOIN students s ON eh.student_id = s.id
+        WHERE eh.room_no = %s AND eh.is_occupied = 1
+    """
+    cursor.execute(query, (room_no,))
+    students = cursor.fetchall()
+    return render_template('room_students.html', students=students, room_no=room_no)
+
+
+
+
 
 
 if __name__=='__main__':
